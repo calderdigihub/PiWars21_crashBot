8a445d3f8c5aff7a8b211427560e91d1c8e4845d
DobbyJ <71317091+DobbyJ@users.noreply.github.com>
2021-06-18 14:55:16 +0100

    Update word_relay.py

--- word_relay.py
+++ word_relay.py
@@ -6,6 +6,31 @@ import speech_recognition as sr
 import directions
 r=sr.Recognizer()
 mic = sr.Microphone() 
+with mic as source:
+	r.adjust_for_ambient_noise(source)
+	print("listening")
+	audio_text = r.listen(source)
+	print("Text: "+r.recognize_google(audio_text))
+	print("not listening")
+	try:
+    	# using google speech recognition
+		code = r.recognize_google(audio_text)
+    	print("Text: "+code)
+		if 
+	except:
+    	print("Sorry, I did not get that")
+		code="blank"
+	if code == "forward":
+		directions.forward()
+	elif code == "reverse"
+		directions.back()
+	elif code == "right":
+		directions.right()
+	elif code == "left":
+		directions.left()
+	elif code == "stop":
+		directions.stop()
+
 while r.recognize_google(audio_text) != "end":
 	with mic as source:
 		r.adjust_for_ambient_noise(source)

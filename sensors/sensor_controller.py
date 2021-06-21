8dfd8df9717175b62c894b33d80840db06bc4273
DobbyJ <71317091+DobbyJ@users.noreply.github.com>
2021-06-21 09:53:55 +0100

    Create sensor_controller.py

---
+++ sensors/sensor_controller.py
@@ -0,0 +1,10 @@
+from sensormodule import isSensorLeft, isSensorRight
+from directions import left,right,forward, left_forward,right_forward
+
+while True:
+    if isSensorLeft() == True:
+        right_forward()
+    elif isSensorRight() == True:
+        left_forward()
+    else:
+        forward()

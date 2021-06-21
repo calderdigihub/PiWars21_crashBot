d28553f02512625c6a8a8f462ed7d51240a76d62
DobbyJ <71317091+DobbyJ@users.noreply.github.com>
2021-06-21 11:36:33 +0100

    Update word_relay.py

--- word_relay.py
+++ word_relay.py
@@ -19,39 +19,116 @@ with mic as source:
 	except:
 		print("Sorry, I did not get that")
 		code="blank"
-	if code == "forward":
+	if code == "forward long":
 		directions.forward()
-	elif code == "reverse"
+		sleep(10)
+		directions.stop()
+	elif code == "forward middle":
+		directions.forward()
+		sleep(5)
+		directions.stop()
+	elif code == "forward short":
+		directions.forward()
+		sleep(1)
+		directions.stop()
+	elif code == "reverse long":
 		directions.back()
-	elif code == "right":
-		directions.right()
-	elif code == "left":
-		directions.left()
-	elif code == "stop":
+		sleep(10)
+		directions.stop()
+	elif code == "reverse middle":
+		directions.back()
+		sleep(5)
+		directions.stop()
+	elif code == "reverse short":
+		directions.back()
+		sleep(1)
+		directions.stop()
+	elif code == "right long":
+		directions.left_forward()
+		sleep(10)
+		directions.stop()
+	elif code == "right middle":
+		directions.left_forward()
+		sleep(5)
+		directions.stop()
+	elif code == "right short":
+		directions.left_forward()
+		sleep(1)
+		directions.stop()
+	elif code == "left long":
+		directions.right_forward()
+		sleep(10)
+		directions.stop()
+	elif code == "left middle":
+		directions.right_forward()
+		sleep(5)
+		directions.stop()
+	elif code == "left short":
+		directions.right_forward()
+		sleep(1)
 		directions.stop()
 
 while r.recognize_google(audio_text) != "end":
-	with mic as source:
-		r.adjust_for_ambient_noise(source)
-		print("listening")
-		audio = r.listen(source)
-		print("Text: "+r.recognize_google(audio_text))
-		print("not listening")
+		with mic as source:
+			r.adjust_for_ambient_noise(source)
+			print("listening")
+			audio = r.listen(source)
+			print("Text: "+r.recognize_google(audio_text))
+			print("not listening")
 
-	try:
-    	# using google speech recognition
-		code = r.recognize_google(audio_text)
-		print("Text: "+code)
-	except:
-		print("Sorry, I did not get that")
-		code="blank"
-	if code == "forward":
-		directions.forward()
-	elif code == "reverse"
-		directions.back()
-	elif code == "right":
-		directions.right()
-	elif code == "left":
-		directions.left()
-	elif code == "stop":
+		try:
+    			# using google speech recognition
+			code = r.recognize_google(audio_text)
+			print("Text: "+code)
+		except:
+			print("Sorry, I did not get that")
+			code="blank"
+		if code == "forward long":
+			directions.forward()
+			sleep(10)
+			directions.stop()
+		elif code == "forward middle":
+			directions.forward()
+			sleep(5)
 		directions.stop()
+		elif code == "forward short":
+			directions.forward()
+			sleep(1)
+			directions.stop()
+		elif code == "reverse long":
+			directions.back()
+			sleep(10)
+			directions.stop()
+		elif code == "reverse middle":
+			directions.back()
+			sleep(5)
+			directions.stop()
+		elif code == "reverse short":
+			directions.back()
+			sleep(1)
+			directions.stop()
+		elif code == "right long":
+			directions.left_forward()
+			sleep(10)
+			directions.stop()
+		elif code == "right middle":
+			directions.left_forward()
+			sleep(5)
+			directions.stop()
+		elif code == "right short":
+			directions.left_forward()
+			sleep(1)
+			directions.stop()
+		elif code == "left long":
+			directions.right_forward()
+			sleep(10)
+			directions.stop()
+		elif code == "left middle":
+			directions.right_forward()
+			sleep(5)
+			directions.stop()
+		elif code == "left short":
+			directions.right_forward()
+			sleep(1)
+			directions.stop()
+
